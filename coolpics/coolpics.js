7e83838c993f38134f3376ef0a01a5a2cd11aafa
kelseyschae <kelsschaefermeyer2004@gmail.com>
2025-05-15 10:55:39 -0600

    js added

---
+++ coolpics/coolpics.js
@@ -0,0 +1,48 @@
+const menuButton = document.querySelector(".menu-button");
+const gallery = document.querySelector(".gallery");
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("hide");
+}
+
+function handleResize() {
+  const menu = document.querySelector(".menu");
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
+  } 
+}
+
+handleResize(); 
+
+function viewerTemplate(pic, alt) {
+  return `<div class="viewer">
+    <span class="close-viewer">X</span>
+    <img src="${pic}" alt="${alt}">
+    </div>`;
+} 
+
+function closeViewer() {
+  document.querySelector(".viewer")?.remove();
+}
+
+function viewHandler(event) {
+  const target = event.target;
+  console.dir(target);
+  const imgSrc = event.target.src.split("-");
+  const newSrc = imgSrc[0] + "-full.jpeg";
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    viewerTemplate(newSrc, target.alt)
+  );
+ 
+  document
+    .querySelector(".close-viewer")
+    .addEventListener("click", closeViewer);
+}
+
+menuButton.addEventListener("click", toggleMenu);
+gallery.addEventListener("click", viewHandler);
+window.addEventListener("resize", handleResize); 
