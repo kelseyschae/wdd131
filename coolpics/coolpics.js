f9dc9f3e6453681e0a114b6e06fb0127512070a4
kelseyschae <kelsschaefermeyer2004@gmail.com>
2025-05-17 20:37:43 -0600

    changed

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -12,97 +12,37 @@ function handleResize() {
     menu.classList.remove("hide");
   } else {
     menu.classList.add("hide");
-  } 
-} 
+  }
+}
 
-handleResize(); 
+handleResize();
 
-function viewerTemplate(pic1, alt1) {
+function viewerTemplate(pic, alt) {
   return `<div class="viewer">
     <span class="close-viewer">X</span>
-    <img src="${pic1}" alt="${alt1}">
+    <img src="${pic}" alt="${alt}">
     </div>`;
-} 
-function viewerTemplate(pic2, alt2) {
-    return `<div class="viewer">
-      <span class="close-viewer">X</span>
-      <img src="${pic2}" alt="${alt2}">
-      </div>`;
-  } 
+}
 
-  function viewerTemplate(pic3, alt3) {
-    return `<div class="viewer">
-      <span class="close-viewer">X</span>
-      <img src="${pic3}" alt="${alt3}">
-      </div>`;
-  } 
-
-  function viewerTemplate(pic4, alt4) {
-    return `<div class="viewer">
-      <span class="close-viewer">X</span>
-      <img src="${pic4}" alt="${alt4}">
-      </div>`;
-  } 
-
-  function viewerTemplate(pic5, alt5) {
-    return `<div class="viewer">
-      <span class="close-viewer">X</span>
-      <img src="${pic5}" alt="${alt5}">
-      </div>`;
-  } 
-
-  function viewerTemplate(pic6, alt6) {
-    return `<div class="viewer">
-      <span class="close-viewer">X</span>
-      <img src="${pic6}" alt="${alt6}">
-      </div>`;
-  } 
-
-  function viewerTemplate(pic7, alt7) {
-    return `<div class="viewer">
-      <span class="close-viewer">X</span>
-      <img src="${pic7}" alt="${alt7}">
-      </div>`;
-  } 
-
-  function viewerTemplate(pic8, alt8) {
-    return `<div class="viewer">
-      <span class="close-viewer">X</span>
-      <img src="${pic8}" alt="${alt8}">
-      </div>`;
-  } 
-
-  function viewerTemplate(pic9, alt9) {
-    return `<div class="viewer">
-      <span class="close-viewer">X</span>
-      <img src="${pic9}" alt="${alt9}">
-      </div>`;
-  } 
-
+function closeViewer() {
+  document.querySelector(".viewer")?.remove();
+}
 
 function viewHandler(event) {
   const target = event.target;
   console.dir(target);
   const imgSrc = event.target.src.split("-");
   const newSrc = imgSrc[0] + "-full.jpeg";
-
   document.body.insertAdjacentHTML(
     "afterbegin",
     viewerTemplate(newSrc, target.alt)
-  ); 
- 
+  );
+
   document
     .querySelector(".close-viewer")
-    .addEventListener("click", closeViewer); 
-} 
-
-add.Modal(events);
-modal.addEventListener('click', events)
-
-function closeViewer() {
-    document.querySelector(".viewer")?.remove(); 
-  } 
+    .addEventListener("click", closeViewer);
+}
 
 menuButton.addEventListener("click", toggleMenu);
-gallery.adEventListener("click", viewHandler);
-window.addEventListener("click",handleResize)
+gallery.addEventListener("click", viewHandler);
+window.addEventListener("resize", handleResize);
