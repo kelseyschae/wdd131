11dcdbceaaf56650703ece1b6a60ceaf900f6c42
kelseyschae <kelsschaefermeyer2004@gmail.com>
2025-05-30 14:10:33 -0600

    changes

--- blog/blog.js
+++ blog/blog.js
@@ -25,48 +25,49 @@ const articles = [
       stars: "⭐⭐⭐⭐"
     },
     {
-        id: 3,
-        title: "Belgariad Book One: Pawn of Prophecy",
-        date: "Feb 12, 2022",
-        description:
+      id: 3,
+      title: "Belgariad Book One: Pawn of Prophecy",
+      date: "Feb 12, 2022",
+      description:
         "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
-        imgSrc:
+      imgSrc:
         "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
-        imgAlt: "Book cover for Pawn of Prophecy",
-        ages: "12-16",
-        genre: "Fantasy",
-        stars: "⭐⭐⭐⭐⭐"
-        }
+      imgAlt: "Book cover for Pawn of Prophecy",
+      ages: "12-16",
+      genre: "Fantasy",
+      stars: "⭐⭐⭐⭐⭐"
+    }
   ];
   
   function articleTemplate(item) {
-    return `<article><section class="post-details">
-      <p class="post-details__date">${item.date}</p>
-      <p>${item.ages}</p>
-      <p>${item.genre}</p>
-      <p>${item.stars}</p>
-    </section>
-    <section class="post-content">
-      <h2>
-        <a href="#">${item.title}</a>
-      </h2>
-      <figure>
-        <img
-          src="${item.imgSrc}"
-          alt="${item.imgAlt}"
-        />
-      </figure>
-      <p>
-      ${item.description}
-        <a href="#">Read More...</a>
-      </p>
-    </section></article>`;
+    return `
+    <article>
+      <section class="post-details">
+        <p class="post-details__date">${item.date}</p>
+        <p>${item.ages}</p>
+        <p>${item.genre}</p>
+        <p>${item.stars}</p>
+      </section>
+      <section class="post-content">
+        <h2>
+          <a href="#">${item.title}</a>
+        </h2>
+        <figure>
+          <img src="${item.imgSrc}" alt="${item.imgAlt}" />
+        </figure>
+        <p>
+          ${item.description}
+          <a href="#">Read More...</a>
+        </p>
+      </section>
+    </article>`;
   }
-  function renderArticleList(list) {
-    const articlesElement = document.querySelector(".articles");
-    const htmlStrings = list.map(articleTemplate);
   
+  function renderArticleList(list) {
+    const articlesElement = document.querySelector("#articles-list");
+    const htmlStrings = list.map(articleTemplate);
     articlesElement.innerHTML = htmlStrings.join("");
   }
   
   renderArticleList(articles);
+  
